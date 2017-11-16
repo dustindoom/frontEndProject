2a10dd32901f42d4ea780199b61cb6b1a59ec522
dustindoom <33705315+dustindoom@users.noreply.github.com>
2017-11-15 19:09:29 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 
 //clicky button go bye bye
 
-
+//Hide contact
 $(".contactMe span").hide();
 $(".contactMe").append("<button>If you would like to contact me click here!</button>");
 $("button").click(function(){
+//Show contact
 $(".contactMe span").show();
 $("button").remove();
 });
